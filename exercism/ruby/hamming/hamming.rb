4b12b1413142a4d8c3e5de57bc26836338ebbc6f
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2014-12-23 22:49:33 -0500

    Start gigasecond and update hamming

--- exercism/ruby/hamming/hamming.rb
+++ exercism/ruby/hamming/hamming.rb
@@ -1,12 +1,10 @@
 class Hamming
-  def self.compute(strand1, strand2)
-    strand_length = strand1.length
+  def self.compute(str1, str2)
     distance = 0
-    strand_length.times do |num|
-      distance += ( strand1[num] == strand2[num] ? 0 : 1 )
-      #distance += self.compare(strand1[num], strand2[num])
+    str2.length.times do |num|
+      distance += ( str1[num] == str2[num] ? 0 : 1 )
     end
-    return distance
+    distance
   end
 end
 
