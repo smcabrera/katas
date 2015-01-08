3e694ba1affb260cec3260efc95993a8cd61dc42
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-01-08 15:07:20 -0500

    Starting javascript exercise

--- exercism/javascript/bob/bob.js
+++ exercism/javascript/bob/bob.js
@@ -6,9 +6,21 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  if (input == ''){
+    return 'Fine. Be that way!'
+  }
+  else {
+    return 'Whatever.'
+  }
 };
+
+//Bob answers 'Sure.' if you ask him a question.
+
+//He answers 'Whoa, chill out!' if you yell at him.
+
+//He says 'Fine. Be that way!' if you address him without actually saying
+//anything.
+
+//He answers 'Whatever.' to anything else.
 
 module.exports = Bob;
