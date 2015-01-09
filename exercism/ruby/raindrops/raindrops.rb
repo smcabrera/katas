bb213168482cd730609f421b133de6fd41e49601
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-01-09 01:10:44 -0500

    Remove unneeded comments

--- exercism/ruby/raindrops/raindrops.rb
+++ exercism/ruby/raindrops/raindrops.rb
@@ -12,9 +12,3 @@ class Raindrops
     end
   end
 end
-
-#- If the number contains 3 as a prime factor, output 'Pling'.
-#- If the number contains 5 as a prime factor, output 'Plang'.
-#- If the number contains 7 as a prime factor, output 'Plong'.
-#- If the number does not contain 3, 5, or 7 as a prime factor,
-  #just pass the number's digits straight through.
