b1284c31f5a302e5e480a49674f5b88254ba153d
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-01-13 14:58:03 -0600

    Start the exercises in python

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -2,5 +2,14 @@
 # Skeleton file for the Python "Bob" exercise.
 #
 def hey(what):
+    if what == '':
+        response = 'Fine. Be that way!'
+    elif '!' in what:
+        response = 'Whoa, chill out!'
+    elif '?' in what:
+        response = 'Sure.'
+    else:
+        response = 'Whatever.'
+    return response
 
-    return
+# I'm going to need access to the internet and some python documentation to finish this one...
