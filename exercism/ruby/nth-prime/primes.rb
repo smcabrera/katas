370f637b0f007d75458005349bffa259c79474a4
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-01-21 09:45:16 -0500

    First passing iteration

--- exercism/ruby/nth-prime/primes.rb
+++ exercism/ruby/nth-prime/primes.rb
@@ -2,13 +2,15 @@ require 'prime'
 
 class Primes
   def self.nth(n)
-    if n == 0 then raise ArgumentError end
     results_array = []
     i = 0
+
+    if n == 0 then raise ArgumentError end
     while results_array.size < n
       i += 1
       if i.prime? then results_array << i end
     end
+
     results_array[n-1]
   end
 end
