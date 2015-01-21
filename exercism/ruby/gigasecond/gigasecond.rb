404d6a26c1f4204c016606d617ee9e71aeeaac75
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-01-21 10:03:18 -0500

    Second iteration

--- exercism/ruby/gigasecond/gigasecond.rb
+++ exercism/ruby/gigasecond/gigasecond.rb
@@ -2,7 +2,12 @@ require 'date'
 require 'time'
 
 class Gigasecond
+  GIGASECOND_IN_SECONDS = 10 ** 9
+
   def self.from(date)
-    (date.to_time + 10 ** 9).to_date
+    given_date_in_seconds = date.to_time
+    result_date_in_seconds = given_date_in_seconds + GIGASECOND_IN_SECONDS
+    result_date_in_seconds.to_date
   end
+
 end
