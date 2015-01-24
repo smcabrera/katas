452aa0a3c8c56c1ab9b23d4f145cd8ec69db8d0c
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-01-24 18:11:24 -0500

    New exercise

---
+++ exercism/ruby/word-count/phrase.rb
@@ -0,0 +1,28 @@
+class Phrase
+  attr_accessor :phrase
+
+  PUNCTUATION = '!@#$%^&*()'
+
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def word_count
+    word_hash = {}
+    self.word_list.each do |word|
+      word_hash.include?(word) ? word_hash[word] += 1 : word_hash[word] = 1
+    end
+    return word_hash
+  end
+
+  def self.word_list(phrase)
+    phrase.split.each do |word|
+      self.remove_punctuation(word)
+    end
+  end
+
+  def self.remove_punctuation(string)
+    PUNCTUATION.each_char { |char| string.sub!(char, '')  }
+    return string
+  end
+end
