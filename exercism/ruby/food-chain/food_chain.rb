c763aebd0d53de06cd6d003d71eb8ae47d83abcb
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-01-29 14:40:38 -0500

    Extract animal data into a single array of hashes

--- exercism/ruby/food-chain/food_chain.rb
+++ exercism/ruby/food-chain/food_chain.rb
@@ -1,7 +1,16 @@
 require 'pry'
 
 class FoodChainSong
-  ANIMALS = ["?", "fly", "spider", "bird", "cat", "dog", "goat", "cow", "horse"]
+  ANIMALS = [              "",
+                           {:name => "fly",    :intro => ""                                                  },
+                           {:name => "spider", :intro => "It wriggled and jiggled and tickled inside her.\n" },
+                           {:name => "bird",   :intro => "How absurd to swallow a bird!\n"                   },
+                           {:name => "cat",    :intro => "Imagine that, to swallow a cat!\n"                 },
+                           {:name => "dog",    :intro => "What a hog, to swallow a dog!\n"                   },
+                           {:name => "goat",   :intro => "Just opened her throat and swallowed a goat!\n"    },
+                           {:name => "cow",    :intro => "I don't know how she swallowed a cow!\n"           },
+                           {:name => "horse",  :intro => "She's dead, of course!\n"                          }
+  ]
 
   def sing
     self.verses(1, 8)
@@ -12,38 +21,28 @@ class FoodChainSong
   end
 
   def verse(n)
-    verse_string = introduce_animal(ANIMALS[n])
+    verse_string = introduce_animal(n)
     if n == 8 then return verse_string end
     verse_string += why_swallow_animals(n)
   end
 
-  def introduce_animal(animal)
-    result = "I know an old lady who swallowed a #{animal}.\n"
-    animal_introductions = { :fly    => "",
-                             :spider => "It wriggled and jiggled and tickled inside her.\n",
-                             :bird   => "How absurd to swallow a bird!\n",
-                             :cat    => "Imagine that, to swallow a cat!\n",
-                             :dog    => "What a hog, to swallow a dog!\n",
-                             :goat   => "Just opened her throat and swallowed a goat!\n",
-                             :cow    => "I don't know how she swallowed a cow!\n",
-                             :horse  => "She's dead, of course!\n" }
-    result += animal_introductions[animal.to_sym]
+  def introduce_animal(n)
+    animal = ANIMALS[n]
+    result = "I know an old lady who swallowed a #{animal[:name]}.\n"
+    result += animal[:intro]
   end
 
-  def why_swallow_this_animal(animal_1, animal_2)
-    spider = animal_2 == "spider" ? " that wriggled and jiggled and tickled inside her" : ""
-    fly = animal_1 == "fly" ? "I don't know why she swallowed the fly. Perhaps she'll die.\n" : nil
-    if fly
-      return fly
-    else
-      return "She swallowed the #{animal_1} to catch the #{animal_2}#{spider}.\n"
-    end
+  def why_swallow_this_animal(n)
+    if n == 1 then return "I don't know why she swallowed the fly. Perhaps she'll die.\n" end
+    animal_1, animal_2 = ANIMALS[n], ANIMALS[n-1]
+    spider = animal_2[:name] == "spider" ? " that wriggled and jiggled and tickled inside her" : ""
+    "She swallowed the #{animal_1[:name]} to catch the #{animal_2[:name]}#{spider}.\n"
   end
 
   def why_swallow_animals(n)
     result = ""
     (1..n).to_a.reverse.each do |i|
-      result += why_swallow_this_animal(ANIMALS[i], ANIMALS[i-1])
+      result += why_swallow_this_animal(i)
     end
     result
   end
