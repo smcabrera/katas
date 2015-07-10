a73929fd459e9190b4bafec6d2d4e7782f08390c
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-07-10 12:19:00 -0400

    Fixing style offenses

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -2,16 +2,14 @@ class Binary
   attr_accessor :value
 
   def initialize(value)
-    @value = value.match("^[01]*$") ? value : '0'
+    @value = value.match('^[01]*$') ? value : '0'
   end
 
   def to_decimal
     result = 0
-    value.split("").each_with_index { |item, index|
-      result += item.to_i * (2 ** (value.length - index - 1))
-    }
+    value.split('').each_with_index do |item, index|
+      result += item.to_i * (2**(value.length - index - 1))
+    end
     result
   end
 end
-
-
